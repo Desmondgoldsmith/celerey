8561ae72af87ce9bc5efa53f17ccd48b86744d44
nandhakumar-rs <rsnk2013@gmail.com>
2025-02-13 01:41:49 +0530

    fix: onboarding screen net worth screen

--- src/Features/onboarding/components/templates/financialInfoTemplates/networthScreen.tsx
+++ src/Features/onboarding/components/templates/financialInfoTemplates/networthScreen.tsx
@@ -1,33 +1,35 @@
-import { Button } from "@/components/ui/button";
-import { useAuthStore } from "@/Features/auth/state";
-import { useOnboardingStore } from "@/Features/onboarding/state";
-import formatCurrency from "@/utils/formatCurrency";
-import React, { useEffect, useState } from "react";
+import { Button } from '@/components/ui/button'
+import { useAuthStore } from '@/Features/auth/state'
+import { useOnboardingStore } from '@/Features/onboarding/state'
+import formatCurrency from '@/utils/formatCurrency'
+import React, { useEffect, useState } from 'react'
 
 interface NetWorthScreenProps {
-  onContinue: () => void;
-  onBack: () => void;
+  onContinue: () => void
+  onBack: () => void
 }
 
 export const NetWorthScreen = ({ onContinue, onBack }: NetWorthScreenProps) => {
-  const {formData} = useOnboardingStore()
-  const {user} = useAuthStore()
+  const { formData } = useOnboardingStore()
+  const { user } = useAuthStore()
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    onContinue();
-  };
+    e.preventDefault()
+    onContinue()
+  }
 
   return (
     <form onSubmit={handleSubmit} className="text-center max-w-xl mx-auto">
       <h1 className="text-4xl font-cirka mb-6">
         Thank You
-        <span className="text-navyLight"> {(user?.firstName + ' ' + user?.lastName) || "User"}</span>, based on
-        the information submitted we estimate your net worth to be{" "}
-        <span className="text-navyLight"> {formatCurrency(
-            formData?.financial?.netWorth || '0',
-            formData.financial?.currency?.split('(')[1].split(')')[0] || 'usd',
-          )}</span>
+        <span className="text-navyLight">
+          {' '}
+          {user?.firstName + ' ' + user?.lastName || 'User'}
+        </span>
+        , based on the information submitted we estimate your net worth to be{' '}
+        <span className="text-navyLight">
+          {formatCurrency(formData?.financial?.netWorth || '0', 'usd')}
+        </span>
       </h1>
       <p className=" mb-12 font-helvetica text-sm">
         Does this look right? If not, please go back and make some adjustments.
@@ -44,5 +46,5 @@ export const NetWorthScreen = ({ onContinue, onBack }: NetWorthScreenProps) => {
         </Button>
       </div>
     </form>
-  );
-};
+  )
+}
